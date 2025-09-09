1e12ff47ea4fbfeeebc4c714a222438cf3a0b2a2
PapsBurr <npons3932@gmail.com>
2025-09-08 19:57:29 -0600

    fix variable name

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -6,7 +6,7 @@ export const apiLimiter = rateLimiter({
   message: 'Too many requests from this IP, please try again later.'
 });
 
-export const nassaLimiter = rateLimiter({
+export const nasaLimiter = rateLimiter({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 50, // Limit each IP to 50 requests per windowMs
   message: 'Too many requests to NASA API from this IP, please try again later.'

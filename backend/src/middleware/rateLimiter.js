72c692c267b54a7189dec8222fb3c10c362cd546
PapsBurr <npons3932@gmail.com>
2025-10-10 18:15:56 -0600

    remove different rate limits for test environment

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,10 +1,8 @@
 import rateLimiter from 'express-rate-limit';
 
-const isTest = process.env.NODE_ENV === 'test';
-
 export const apiLimiter = rateLimiter({
-  windowMs: isTest ? 1000 : 15 * 60 * 1000, // 15 minutes
-  max: isTest ? 5 : 100, 
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
   handler: (req, res, next) => {
     const error = new Error();
     error.name = 'RateLimitError';
@@ -15,8 +13,8 @@ export const apiLimiter = rateLimiter({
 });
 
 export const nasaLimiter = rateLimiter({
-  windowMs: isTest ? 1000 : 60 * 60 * 1000, // 1 hour
-  max: isTest ? 5 : 50, 
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 50,
   handler: (req, res, next) => {
     const error = new Error();
     error.name = 'RateLimitError';

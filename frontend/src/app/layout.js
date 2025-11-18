5360dabcd52ec7ab15366f1fcc794469ee753cb8
PapsBurr <npons3932@gmail.com>
2025-11-18 13:29:07 -0700

    Fix formatting

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -25,9 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );

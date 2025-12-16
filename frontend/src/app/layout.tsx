0be4d60e789581335a38cfc42a8f951c5dd27583
PapsBurr <npons3932@gmail.com>
2025-12-15 20:13:57 -0700

    refactor: change to typescript

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,13 +18,16 @@ export const metadata = {
   description: "A static website showcasing my projects and qualifications.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>

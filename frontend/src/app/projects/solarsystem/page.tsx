2ca89737cbc2e538a68019c4af33f63a2609c761
Nathan <npons393@gmail.com>
2025-12-26 01:19:33 +0000

    feat: add the base scene for the 3d env

--- frontend/src/app/projects/solarsystem/page.tsx
+++ frontend/src/app/projects/solarsystem/page.tsx
@@ -1,13 +1,14 @@
-'use client'
-import dynamic from 'next/dynamic'
+"use client";
+import dynamic from "next/dynamic";
 
 // Disable SSR for the 3D component
-const SolarSystemScene = dynamic(() => import('@/app/components/SolarSystemScene'), {
-  ssr: false,
-})
+const SolarSystemScene = dynamic(
+  () => import("@/app/components/SolarSystemScene"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
-  return (
-    <SolarSystemScene />
-  )
+  return <SolarSystemScene />;
 }

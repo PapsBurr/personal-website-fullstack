b05ab52a1643be6f1d9524167f16977dc1a928ba
PapsBurr <npons3932@gmail.com>
2025-12-31 20:42:26 -0700

    feat: move planet data from solarsystemscene to planetData

---
+++ frontend/src/app/components/solarsystem/planetData.ts
@@ -0,0 +1,87 @@
+import { PlanetData, RotationDirection } from "./interfaces";
+
+// Real data: distance is km from the sun
+export const planetData: Record<string, PlanetData> = {
+  sun: {
+    name: "Sun",
+    distanceKm: 0,
+    orbitalPeriodHrs: 0,
+    rotationPeriodHrs: 609.12,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 696340,
+  },
+  mercury: {
+    name: "Mercury",
+    distanceKm: 57909050,
+    orbitalPeriodHrs: 2111.28,
+    rotationPeriodHrs: 1407.6,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 2439.7,
+  },
+  venus: {
+    name: "Venus",
+    distanceKm: 108208000,
+    orbitalPeriodHrs: 5392.8,
+    rotationPeriodHrs: 5832.5,
+    rotationDirection: RotationDirection.CLOCKWISE,
+    radiusKm: 6051.8,
+  },
+  earth: {
+    name: "Earth",
+    distanceKm: 149598023,
+    orbitalPeriodHrs: 8766.24,
+    rotationPeriodHrs: 23.93,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 6371,
+    satellites: {
+      moon: {
+        name: "Moon",
+        distanceKm: 384400,
+        orbitalPeriodHrs: 655.2,
+        rotationPeriodHrs: 655.2,
+        rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+        radiusKm: 1737.1,
+      },
+    },
+  },
+  mars: {
+    name: "Mars",
+    distanceKm: 227939366,
+    orbitalPeriodHrs: 16488,
+    rotationPeriodHrs: 24.62,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 3389.5,
+  },
+  jupiter: {
+    name: "Jupiter",
+    distanceKm: 778479000,
+    orbitalPeriodHrs: 103944,
+    rotationPeriodHrs: 9.93,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 69911,
+  },
+  saturn: {
+    name: "Saturn",
+    distanceKm: 1432000000,
+    orbitalPeriodHrs: 257928,
+    rotationPeriodHrs: 10.56,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 58232,
+  },
+  uranus: {
+    name: "Uranus",
+    distanceKm: 2867000000,
+    orbitalPeriodHrs: 734136,
+    rotationPeriodHrs: 17.24,
+    rotationDirection: RotationDirection.CLOCKWISE,
+    radiusKm: 25362,
+  },
+  neptune: {
+    name: "Neptune",
+    distanceKm: 4515000000,
+    orbitalPeriodHrs: 1435200,
+    rotationPeriodHrs: 16.11,
+    rotationDirection: RotationDirection.COUNTERCLOCKWISE,
+    radiusKm: 24622,
+  },
+};

cffe87ced628c57ff6a3696b997f76db8ba8d35e
Nathan Pons <npons393@gmail.com>
2026-01-26 13:23:07 -0700

    chore: update comment to trigger github actions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -28,7 +28,7 @@ const pgrmLanguages: SkillItem[] = [
   { key: 9, text: "UML" },
 ];
 
-// change this comment to cause github actions to run : 16
+// change this comment to cause github actions to run : 17
 
 function ListItems(props: SkillItem) {
   return <li>{props.text}</li>;

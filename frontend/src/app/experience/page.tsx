b4d35be752bb05e221e9d402167b725f4f9e78d9
PapsBurr <npons3932@gmail.com>
2026-03-24 19:44:20 -0600

    feat: add beginnings of the experience page

---
+++ frontend/src/app/experience/page.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import RoundedImage from "../components/RoundedImage";
+
+export default function Page() {
+  return (
+    <>
+      <div className="page-shell">
+        <main className="page-main">
+          {/* Opening Section */}
+          <section className="fade-trigger">
+            <h1>Experience</h1>
+          </section>
+
+          {/* WordPress Section */}
+          <section className="fade-trigger">
+            <h2>WordPress - Elementor</h2>
+            <p>
+              Early in my web dev career, I collaborated remotely with a small
+              team to redesign and rebuild a website for a flooring business in
+              Jacksonville, Florida. Working together from different locations,
+              we created a new website for them with fresh branding and a new
+              design.
+            </p>
+            <p>
+              I was responsible for actually building the site. I handled the
+              layout of the site and creating the flow for the user, ensuring a
+              smooth and intuitive experience.
+            </p>
+            <div className="my-8 flex flex-wrap gap-4">
+              <div>
+                <Link
+                  href="https://amazingfloorsandmore.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="primary-button"
+                >
+                  Visit the Website
+                </Link>
+              </div>
+            </div>
+
+            {/* Image of the website */}
+            <div className="flex justify-center mt-12">
+              <RoundedImage
+                src="/afam-homepage-full.png"
+                alt="An image of the Amazing Floors & More homepage."
+                width={1000}
+              />
+            </div>
+
+            {/* Walkthrough of the site */}
+            <section className="fade-trigger">
+              <h3>Walkthrough</h3>
+              <p>
+                The homepage features a hero section with a clear call to
+                action, followed by sections that highlight the company's
+                services, showcase their work, and provide contact information.
+                The design is clean and modern, with a focus on usability and
+                accessibility.
+              </p>
+            </section>
+
+            {/* Things I did */}
+
+            {/* Custom Components and thought process */}
+
+            {/* Working with the clients */}
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}

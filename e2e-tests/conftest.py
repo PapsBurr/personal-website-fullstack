0f025aa6c31db3444913d651d09621250ce709c9
PapsBurr <npons3932@gmail.com>
2026-04-09 14:30:32 -0600

    feat: add fixture for the session wide selenium driver

---
+++ e2e-tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+BASE_URL = os.environ.get("BASE_URL", "http://localhost:8000")
+
+
+@pytest.fixture(scope="session")
+def driver():
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()

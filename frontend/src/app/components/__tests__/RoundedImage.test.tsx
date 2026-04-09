8d4790c00c26fd74793ee2a735536568f26187fa
PapsBurr <npons3932@gmail.com>
2026-04-09 13:57:16 -0600

    fix: update test to have the right src attribute

--- frontend/src/app/components/__tests__/RoundedImage.test.tsx
+++ frontend/src/app/components/__tests__/RoundedImage.test.tsx
@@ -1,21 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import RoundedImage from '../RoundedImage';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import RoundedImage from "../RoundedImage";
 
-describe('RoundedImage Component', () => {
-  it('renders the image with correct src, alt text, width and height', () => {
-    render(<RoundedImage src="/test-image.png" alt="Test Image" width={100} height={100}/>);
-    const image = screen.getByRole('img', { name: /Test Image/i });
+describe("RoundedImage Component", () => {
+  it("renders the image with correct src, alt text, width and height", () => {
+    render(
+      <RoundedImage
+        src="/test-image.png"
+        alt="Test Image"
+        width={100}
+        height={100}
+      />,
+    );
+    const image = screen.getByRole("img", { name: /Test Image/i });
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute('src', '/_next/image?url=%2Ftest-image.png&w=256&q=75');
-    expect(image).toHaveAttribute('alt', 'Test Image');
-    expect(image).toHaveAttribute('width', '100');
-    expect(image).toHaveAttribute('height', '100');
+    expect(image).toHaveAttribute("src", "/test-image.png");
+    expect(image).toHaveAttribute("alt", "Test Image");
+    expect(image).toHaveAttribute("width", "100");
+    expect(image).toHaveAttribute("height", "100");
   });
 
-  it('applies the correct class for rounded corners', () => {
-    render(<RoundedImage src="/test-image2.png" alt="Test Image2" width={50} height={50}/>);
-    const image = screen.getByRole('img', { name: /Test Image2/i });
-    expect(image).toHaveClass('rounded-[20px] shadow-md/40');
+  it("applies the correct class for rounded corners", () => {
+    render(
+      <RoundedImage
+        src="/test-image2.png"
+        alt="Test Image2"
+        width={50}
+        height={50}
+      />,
+    );
+    const image = screen.getByRole("img", { name: /Test Image2/i });
+    expect(image).toHaveClass("rounded-[20px] shadow-md/40");
   });
 });

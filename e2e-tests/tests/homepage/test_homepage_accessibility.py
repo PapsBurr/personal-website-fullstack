35641d8a20e02895a8d17ece53d317e8da2b83b1
PapsBurr <npons3932@gmail.com>
2026-04-14 19:27:53 -0600

    test: add base test files for planned tests

---
+++ e2e-tests/tests/homepage/test_homepage_accessibility.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from axe_selenium_python import Axe
+from conftest import BASE_URL

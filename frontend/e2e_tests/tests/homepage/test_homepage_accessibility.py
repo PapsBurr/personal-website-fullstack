c704173ecd97823e413ca0505074eddc54979eba
PapsBurr <npons3932@gmail.com>
2026-04-15 19:12:55 -0600

    fix: conftest path in import

--- frontend/e2e_tests/tests/homepage/test_homepage_accessibility.py
+++ frontend/e2e_tests/tests/homepage/test_homepage_accessibility.py
@@ -2,4 +2,4 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from axe_selenium_python import Axe
-from frontend.e2e_tests.conftest import BASE_URL
+from conftest import BASE_URL

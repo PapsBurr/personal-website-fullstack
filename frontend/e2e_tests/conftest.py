c4ab19c2f343b6cfe426adbead71758547540965
PapsBurr <npons3932@gmail.com>
2026-04-15 20:12:41 -0600

    feat: add headless option to driver

--- frontend/e2e_tests/conftest.py
+++ frontend/e2e_tests/conftest.py
@@ -8,7 +8,7 @@ BASE_URL = os.environ.get("BASE_URL", "https://nathanpons.com/")
 @pytest.fixture(scope="session")
 def driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 

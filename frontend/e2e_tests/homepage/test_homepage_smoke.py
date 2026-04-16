83e13f02f04801de7907246018cb580739f54a22
PapsBurr <npons3932@gmail.com>
2026-04-16 10:38:42 -0600

    test: mark smoke tests with pytest smoke decorator

--- frontend/e2e_tests/homepage/test_homepage_smoke.py
+++ frontend/e2e_tests/homepage/test_homepage_smoke.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pytest
 from conftest import BASE_URL
 
 
+@pytest.mark.smoke
 def test_homepage_initialization(driver):
     driver.get(BASE_URL)
     main = WebDriverWait(driver, 10).until(
@@ -12,6 +14,7 @@ def test_homepage_initialization(driver):
     assert main is not None, "Main content area should be present on the homepage"
 
 
+@pytest.mark.smoke
 def test_homepage_has_name(driver):
     driver.get(BASE_URL)
     name = WebDriverWait(driver, 10).until(
@@ -24,6 +27,7 @@ def test_homepage_has_name(driver):
     ), "The homepage should contain my name since it's my portfolio"
 
 
+@pytest.mark.smoke
 def test_homepage_has_title(driver):
     driver.get(BASE_URL)
     title = WebDriverWait(driver, 10).until(
@@ -36,6 +40,7 @@ def test_homepage_has_title(driver):
     ), "The homepage should have my profession title element in the head section"
 
 
+@pytest.mark.smoke
 def test_homepage_has_apod(driver):
     driver.get(BASE_URL)
     apod = WebDriverWait(driver, 10).until(
@@ -46,6 +51,7 @@ def test_homepage_has_apod(driver):
     ), "The homepage should have the APOD section since it's a key feature of the site"
 
 
+@pytest.mark.smoke
 def test_homepage_has_capabilities(driver):
     driver.get(BASE_URL)
     capabilities = WebDriverWait(driver, 10).until(
@@ -58,6 +64,7 @@ def test_homepage_has_capabilities(driver):
     ), "The homepage should have the Capabilities section since it's a key feature of the site"
 
 
+@pytest.mark.smoke
 def test_homepage_has_programming_languages(driver):
     driver.get(BASE_URL)
     programming_languages = WebDriverWait(driver, 10).until(
@@ -70,6 +77,7 @@ def test_homepage_has_programming_languages(driver):
     ), "The homepage should have the Programming Languages section since it's a key feature of the site"
 
 
+@pytest.mark.smoke
 def test_homepage_has_credentials(driver):
     driver.get(BASE_URL)
     credentials = WebDriverWait(driver, 10).until(

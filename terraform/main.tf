2571b65180c32623e90a7d4badc0f8f4d5758266
PapsBurr <npons3932@gmail.com>
2026-04-23 10:39:13 -0600

    refactor: change second_subnet to secondary_subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -557,7 +557,7 @@ resource "aws_subnet" "main_subnet" {
   tags = local.common_tags
 }
 
-resource "aws_subnet" "second_subnet" {
+resource "aws_subnet" "secondary_subnet" {
   vpc_id            = aws_vpc.personal_website_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}b"

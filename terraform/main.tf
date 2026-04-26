0cd7bb64ee9ca06c4d73a7bef9fe00b2f2e19052
PapsBurr <npons3932@gmail.com>
2026-04-25 21:08:21 -0600

    fix: remove private route table resources to disconnect private subnet from the internet gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -670,21 +670,6 @@ resource "aws_route_table_association" "public_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.personal_website_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_igw.id
-  }
-}
-
-resource "aws_route_table_association" "private_route_table_association" {
-  count          = length(aws_subnet.private_subnets)
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.personal_website_vpc.id
 

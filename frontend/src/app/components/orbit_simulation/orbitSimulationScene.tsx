57f94a50bbc57ffd3351b4b1ab5ae6dbc2d62a85
PapsBurr <npons3932@gmail.com>
2026-04-30 10:07:07 -0600

    feat: wrap canvas in canvas-container class

--- frontend/src/app/components/orbit_simulation/orbitSimulationScene.tsx
+++ frontend/src/app/components/orbit_simulation/orbitSimulationScene.tsx
@@ -7,23 +7,25 @@ import * as THREE from "three";
 
 export default function () {
   return (
-    <Canvas
-      style={{ background: "black" }}
-      camera={{ position: [-196, 64, 64], fov: 90, near: 0.01, far: 10000 }}
-      gl={{
-        antialias: true,
-        powerPreference: "high-performance",
-        alpha: false,
-      }}
-      dpr={[1, 1.5]} // Limit pixel ratio
-    >
-      <Suspense fallback={null}>
-        <GizmoHelper alignment="top-left" margin={[80, 80]}>
-          <GizmoViewcube />
-        </GizmoHelper>
-        <OrbitControls enableRotate makeDefault zoomSpeed={5} />
-        <ambientLight intensity={0.2} />
-      </Suspense>
-    </Canvas>
+    <div className="canvas-container">
+      <Canvas
+        style={{ background: "black" }}
+        camera={{ position: [-196, 64, 64], fov: 90, near: 0.01, far: 10000 }}
+        gl={{
+          antialias: true,
+          powerPreference: "high-performance",
+          alpha: false,
+        }}
+        dpr={[1, 1.5]} // Limit pixel ratio
+      >
+        <Suspense fallback={null}>
+          <GizmoHelper alignment="top-left" margin={[80, 80]}>
+            <GizmoViewcube />
+          </GizmoHelper>
+          <OrbitControls enableRotate makeDefault zoomSpeed={5} />
+          <ambientLight intensity={0.2} />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 }
